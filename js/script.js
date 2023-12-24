e2edab02365475949a40d9e61dabc4a957b1cbc5
omardhaif3 <omardhaif3@gmail.com>
2023-12-24 23:06:37 +0530

    update to delete and add

--- js/script.js
+++ js/script.js
@@ -1,31 +1,104 @@
-document.getElementById('fileInput').addEventListener('change', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
+  const fileInput = document.getElementById('fileInput');
+  const dataBlocksContainer = document.getElementById('dataBlocks');
+  const errorElement = document.getElementById('error');
+
+  fileInput.addEventListener('change', handleFileSelect);
+  document.getElementById('addBlock').addEventListener('click', addBlock);
+  document.getElementById('deleteBlock').addEventListener('click', deleteBlock);
+  document.getElementById('saveFile').addEventListener('click', saveFile);
+
+  let jsonData = null;
+
+  function handleFileSelect(event) {
     const file = event.target.files[0];
     if (!file) return;
-  
+
     const reader = new FileReader();
     reader.onload = function(event) {
       const content = event.target.result;
       try {
-        const data = JSON.parse(content);
-        displayJSON(data);
+        jsonData = JSON.parse(content);
+        displayJSON(jsonData);
       } catch (error) {
         displayError('Invalid JSON file');
       }
     };
     reader.readAsText(file);
-  });
-  
+  }
+
   function displayJSON(data) {
-    const outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = '';
-  
-    const formattedData = JSON.stringify(data, null, 2);
-    const formattedHTML = `<pre>${formattedData}</pre>`;
-    outputDiv.innerHTML = formattedHTML;
+    dataBlocksContainer.innerHTML = '';
+    if (!data) return;
+
+    // Displaying the JSON data
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        const block = document.createElement('div');
+        block.classList.add('data-block');
+        block.textContent = `${key}: ${JSON.stringify(data[key])}`;
+        dataBlocksContainer.appendChild(block);
+      }
+    }
   }
-  
+
   function displayError(message) {
-    const outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = `<p id="error">${message}</p>`;
+    errorElement.textContent = message;
   }
-  
+
+  function addBlock() {
+    if (!jsonData) {
+      displayError('Please load a JSON file first');
+      return;
+    }
+
+    // Logic to add a new block
+    const newKey = prompt('Enter new key:');
+    const newValue = prompt('Enter value for the key:');
+    if (newKey && newValue) {
+      jsonData[newKey] = newValue;
+      displayJSON(jsonData);
+    } else {
+      displayError('Invalid input');
+    }
+  }
+
+  function deleteBlock() {
+    if (!jsonData) {
+      displayError('Please load a JSON file first');
+      return;
+    }
+
+    // Logic to delete a block
+    const keyToDelete = prompt('Enter key to delete:');
+    if (keyToDelete && jsonData.hasOwnProperty(keyToDelete)) {
+      delete jsonData[keyToDelete];
+      displayJSON(jsonData);
+    } else {
+      displayError('Key not found');
+    }
+  }
+
+  function saveFile() {
+    if (!jsonData) {
+      displayError('No data to save');
+      return;
+    }
+
+    // Logic to save the JSON file
+    const jsonDataString = JSON.stringify(jsonData, null, 2);
+    const blob = new Blob([jsonDataString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'modified_data.json';
+    document.body.appendChild(link);
+    link.click();
+
+    setTimeout(function() {
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+});
